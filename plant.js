02bda9bfff0848a3a08451141ec69a811daff258
LovelyCat2068 <76818025+LovelyCat2068@users.noreply.github.com>
2022-04-27 19:10:50 +0530

    Add files via upload
    pro-133

---
+++ plant.js
@@ -0,0 +1,40 @@
+pickett: "";
+image: [];
+
+function preload() {
+    image = loadImage('plant.jpg');
+}
+    
+function setup(){
+    canvas = createCanvas(500, 500);
+    canvas.center();
+    magizoologist = ml5.objectDetector("cocossd", modeLLoaded);
+    document.getElementById("teddy").innerHTML = "Status : Detecting Things"
+}
+
+function modeLLoaded(){
+    pickett = true;
+    magizoologist.detect(image, gotResults);
+}
+
+function gotResults(error, resultds){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        images : results;
+    }
+}
+
+function draw(){
+    if(pickett != true){
+        for(i=0, i < pickett.length,i++){
+            confidence = floor(images[i].confidence * 100);
+            label  = images[i].label+confidence+"%";
+            text(label+images[i].x,images[i].y);
+            rect(images[i].x,images[i].y,images[i].width,images[i].height);
+            document.getElementById("teddy").innerHTML = "Status : Objects Detected"
+        }
+    }
+}
